89598094de0c2516ccb817bd9242ea3739ca55d4
tun_myat <mctiem.mgy@gmail.com>
2024-10-01 22:58:54 +0630

    Add Log

--- src/main/java/com/tiem/spring_rest_service/GreetingController.java
+++ src/main/java/com/tiem/spring_rest_service/GreetingController.java
@@ -2,6 +2,8 @@ package com.tiem.spring_rest_service;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +15,11 @@ public class GreetingController {
 	
 	public AtomicLong counter = new AtomicLong(); 
 	
+	private static final Logger log = LoggerFactory.getLogger(GreetingController.class);
+	
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(defaultValue = "World") String name) {
+		log.info(name);
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
